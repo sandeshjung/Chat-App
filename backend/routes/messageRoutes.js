20584c5e7d2ad866b0bce5022c560d1a11d622b0
Sandesh Jung Kunwar <sandeshjung@gmail.com>
2022-11-18 10:41:27 +0545

    Message api

--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { sendMessage } from "../controllers/messageControllers.js";
+import { allMessages, sendMessage } from "../controllers/messageControllers.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.route("/").post(protect, sendMessage);
-// router.route('/:chatId').get(protect,allMessages)
+router.route("/:chatId").get(protect, allMessages);
 
 export default router;
